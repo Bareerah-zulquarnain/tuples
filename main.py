b4dbdd2a3fc6343d45b2396d03ff069ae4af3a2a
Bareerah-zulquarnain <bareerah.zulquarnain@gmail.com>
2024-10-05 14:58:36 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+my_tuples = ()
+print (my_tuples)
+
+my_tuples = (1, 2, 3)
+print (my_tuples)
+
+my_tuples = (1, "hello", 3.4)
+print (my_tuples)
+
+my_tuple = ("mouse " , [8, 4, 6](1, 2, 3))
+print (my_tuples)
+
+my_tuple= ('p',' e', 'r', 'm',' i', 't' )
+print( my_tuple[0] )
+print( my_tuple[5] )
+
+n_tuple = ("mouse " , [8, 4, 6](1, 2, 3))
+print (my_tuples)
+
+print( n_tuple[0][3] )
+print( n_tuple[1][1] )
+
+print("sliced")
+print("Sliced: ", my_tuple[1:4])
+for letter in (my_tuple):
+    print("hello ", letter)
